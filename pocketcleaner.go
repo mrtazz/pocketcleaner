d4577052428f82747a51868f8e48d41302aff0e1
Daniel Schauenberg <d@unwiredcouch.com>
2015-11-24 07:30:38 -0500

    implement FilterOutNewestItems

--- pocketcleaner.go
+++ pocketcleaner.go
@@ -3,50 +3,72 @@
 package pocketcleaner
 
 import (
-  "encoding/json"
+	"encoding/json"
+	"sort"
 )
 
 type PocketItem struct {
-  GivenTitle string      `json:"given_title"`
-  IsArticle string       `json:"is_article"`
-  ResolvedID string      `json:"resolved_id"`
-  Status string          `json:"status"`
-  SortId uint            `json:"sort_id"`
-  HasImage string        `json:"has_image"`
-  Excerpt string         `json:"excerpt"`
-  TimeFavorited string   `json:"time_favorited"`
-  WordCount string       `json:"word_count"`
-  TimeRead string        `json:"time_read"`
-  ResolvedTitle string   `json:"resolved_title"`
-  TimeUpdated string     `json:"time_updated"`
-  Favorite string        `json:"favorite"`
-  HasVideo string        `json:"has_video"`
-  TimeAdded string       `json:"time_added"`
-  ResolvedUrl string     `json:"resolved_url"`
-  GivenUrl string        `json:"given_url"`
-  IsIndex string         `json:"is_index"`
-  ItemID string          `json:"item_id"`
+	GivenTitle    string `json:"given_title"`
+	IsArticle     string `json:"is_article"`
+	ResolvedID    string `json:"resolved_id"`
+	Status        string `json:"status"`
+	SortId        uint   `json:"sort_id"`
+	HasImage      string `json:"has_image"`
+	Excerpt       string `json:"excerpt"`
+	TimeFavorited string `json:"time_favorited"`
+	WordCount     string `json:"word_count"`
+	TimeRead      string `json:"time_read"`
+	ResolvedTitle string `json:"resolved_title"`
+	TimeUpdated   string `json:"time_updated"`
+	Favorite      string `json:"favorite"`
+	HasVideo      string `json:"has_video"`
+	TimeAdded     uint64 `json:"time_added,string"`
+	ResolvedUrl   string `json:"resolved_url"`
+	GivenUrl      string `json:"given_url"`
+	IsIndex       string `json:"is_index"`
+	ItemID        string `json:"item_id"`
+}
+
+// // implement sort interface for PocketItemArray
+type PocketItemArray []PocketItem
+
+func (list PocketItemArray) Len() int {
+	return len(list)
+}
+
+func (list PocketItemArray) Less(i, j int) bool {
+	return list[i].TimeAdded < list[j].TimeAdded
+}
+
+func (list PocketItemArray) Swap(i, j int) {
+	list[i], list[j] = list[j], list[i]
 }
 
 type PocketItemList map[string]PocketItem
 
 type PocketSearchMeta struct {
-  SearchType string `json:"search_type"`
+	SearchType string `json:"search_type"`
 }
 
 type PocketResponse struct {
-  SearchMeta PocketSearchMeta `json:"search_meta"`
-  Status uint                 `json:"status"`
-  Complete uint               `json:"complete"`
-  List PocketItemList         `json:"list"`
-  Error bool                  `json:"error"`
-  Since uint                  `json:"since"`
+	SearchMeta PocketSearchMeta `json:"search_meta"`
+	Status     uint             `json:"status"`
+	Complete   uint             `json:"complete"`
+	List       PocketItemList   `json:"list"`
+	Error      bool             `json:"error"`
+	Since      uint             `json:"since"`
 }
 
 func GetAllPocketItems() {
 }
 
-func FilterOutNewestItems() {
+func FilterOutNewestItems(list PocketItemArray, count int) PocketItemArray {
+	if len(list) < count {
+		return make([]PocketItem, 0)
+	}
+	sort.Sort(list)
+
+	return list[0 : len(list)-count]
 }
 
 func ArchiveItems() {
@@ -56,7 +78,7 @@ func CallPocketAPI(method string, consumer_key string, access_token string) {
 }
 
 func ParsePocketResponse(response string) (PocketResponse, error) {
-  ret := PocketResponse{}
-  err := json.Unmarshal([]byte(response), &ret)
-  return ret, err
+	ret := PocketResponse{}
+	err := json.Unmarshal([]byte(response), &ret)
+	return ret, err
 }
--- pocketcleaner_test.go
+++ pocketcleaner_test.go
@@ -1,25 +1,71 @@
 package pocketcleaner_test
 
 import (
-  "testing"
-  "io/ioutil"
-  "github.com/mrtazz/pocketcleaner"
+	"github.com/mrtazz/pocketcleaner"
+	"io/ioutil"
+	"testing"
 )
 
 func TestParsePacketResponse(t *testing.T) {
-  input, _ := ioutil.ReadFile("fixtures/pocket_response.json")
-  ret, err := pocketcleaner.ParsePocketResponse(string(input))
-  if err != nil {
-    t.Errorf("ParsePocketResponse: parse failed: %s", err.Error())
-  }
-  if ret.Since != 1448244422 {
-    t.Errorf("ParsePocketResponse: expected %d, actual %d", 1448244422, ret.Since)
-  }
-  if len(ret.List) != 16 {
-    t.Errorf("ParsePocketResponse: expected %d, actual %d", 16, len(ret.List))
-  }
-  item := ret.List["839271306"]
-  if item.GivenTitle != "That's What Xu Said : Stop Blowhard Syndrome" {
-    t.Errorf("ParsePocketResponse: expected %s, actual %s", "That's What Xu Said : Stop Blowhard Syndrome", item.GivenTitle)
-  }
+	input, _ := ioutil.ReadFile("fixtures/pocket_response.json")
+	ret, err := pocketcleaner.ParsePocketResponse(string(input))
+	if err != nil {
+		t.Errorf("ParsePocketResponse: parse failed: %s", err.Error())
+	}
+	if ret.Since != 1448244422 {
+		t.Errorf("ParsePocketResponse: expected %d, actual %d", 1448244422, ret.Since)
+	}
+	if len(ret.List) != 16 {
+		t.Errorf("ParsePocketResponse: expected %d, actual %d", 16, len(ret.List))
+	}
+	item := ret.List["839271306"]
+	if item.GivenTitle != "That's What Xu Said : Stop Blowhard Syndrome" {
+		t.Errorf("ParsePocketResponse: expected %s, actual %s", "That's What Xu Said : Stop Blowhard Syndrome", item.GivenTitle)
+	}
+}
+
+func TestFilterOutNewestItems(t *testing.T) {
+	input, _ := ioutil.ReadFile("fixtures/pocket_response.json")
+	items, err := pocketcleaner.ParsePocketResponse(string(input))
+	if err != nil {
+		t.Errorf("ParsePocketResponse: parse failed: %s", err.Error())
+	}
+	arr := make(pocketcleaner.PocketItemArray, 0)
+
+	for _, v := range items.List {
+		arr = append(arr, v)
+	}
+
+	ret := pocketcleaner.FilterOutNewestItems(arr, 5)
+
+	if len(ret) != 11 {
+		t.Errorf("FilterOutNewestItems: expected: %d, actual: %d", 11, len(ret))
+	}
+
+	// TODO: test that the right ones got filtered
+	var flagtests = []struct {
+		id          int
+		timestamp   string
+		given_title string
+	}{
+		{0, "1385319303", "Silicon Allee » Bootstrapping Business: Grow Your Company Without VC Fundin"},
+		{1, "1394546312", "Your Marriage Will Fail, by Alicia Liu | Model View Culture"},
+		{2, "1400039331", ""},
+		{3, "1404071597", "http://www.vox.com/2014/6/26/5837638/the-ipo-is-dying-marc-andreessen-expla"},
+		{4, "1410813125", "published a fascinating writeup"},
+		{5, "1414509648", "Project Managing Your Health — Medium"},
+		{6, "1415733610", "What It's Like To Burn Out - Career Burnout - Elle"},
+		{7, "1421113507", "Towards Better Interviews – Venkata Mahalingam"},
+		{8, "1421515685", "Why Remote Engineering Is So Difficult blog.learningbyshipping.com/2014/12/"},
+		{9, "1422935881", ""},
+		{10, "1426045670", "Meeting with purpose derrickbradley.github.io/2015/02/20/mee…"},
+	}
+
+	for _, tt := range flagtests {
+		if ret[tt.id].GivenTitle != tt.given_title {
+			t.Errorf("FilterOutNewestItems Title: expected: %s, actual: %s, at index: %d", tt.given_title, ret[tt.id].GivenTitle, tt.id)
+			t.Errorf("FilterOutNewestItems Timestamp: expected: %s, actual: %d, at index: %d", tt.timestamp, ret[tt.id].TimeAdded, tt.id)
+		}
+	}
+
 }
